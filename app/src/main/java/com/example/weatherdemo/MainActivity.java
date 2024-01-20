05b1eb8f33118d7f3bdf96cebcfc524ce9c743cd
Piyush Dhiman <83651098+PiyushD1712@users.noreply.github.com>
2024-01-20 22:54:39 +0530

    Second Commit

--- app/src/main/java/com/example/weatherdemo/MainActivity.java
+++ app/src/main/java/com/example/weatherdemo/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -34,12 +36,22 @@ public class MainActivity extends AppCompatActivity {
         imageView = activityMainBinding.imageWeather;
 
         EditText editText = findViewById(R.id.idEdittext);
-        Button btn = findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
+
+        editText.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View v) {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
                 String str = editText.getText().toString();
                 fetchData(str);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
             }
         });
     }
